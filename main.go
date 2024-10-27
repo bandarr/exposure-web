a2e01473c1b7f7e085fd677444470187c35ab4ae
bandarr <bandarr@gmail.com>
2024-10-27 18:25:16 -0400

    fix tab size

--- main.go
+++ main.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-    "exposure-web/rfexposure"
-    "fmt"
-    "html/template"
-    "log"
-    "net/http"
+	"exposure-web/rfexposure"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-    data := rfexposure.TestStub()
-    tmpl := template.Must(template.ParseFiles("form.html"))
-    tmpl.Execute(w, data)
+	data := rfexposure.TestStub()
+	tmpl := template.Must(template.ParseFiles("form.html"))
+	tmpl.Execute(w, data)
 }
 
 func submitHandler(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
-        http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-        return
-    }
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
 
-    // Uncomment and implement the following lines if database functionality is required.
-    // name := r.FormValue("name")
-    // email := r.FormValue("email")
-    // insertSQL := `INSERT INTO users (name, email) VALUES (?, ?)`
-    // _, err := db.Exec(insertSQL, name, email)
-    // if err != nil {
-    // 	http.Error(w, "Unable to save data", http.StatusInternalServerError)
-    // 	return
-    // }
-    // fmt.Fprintf(w, "Form submitted successfully!")
+	// Uncomment and implement the following lines if database functionality is required.
+	// name := r.FormValue("name")
+	// email := r.FormValue("email")
+	// insertSQL := `INSERT INTO users (name, email) VALUES (?, ?)`
+	// _, err := db.Exec(insertSQL, name, email)
+	// if err != nil {
+	// 	http.Error(w, "Unable to save data", http.StatusInternalServerError)
+	// 	return
+	// }
+	// fmt.Fprintf(w, "Form submitted successfully!")
 }
 
 func main() {
-    // Uncomment the following lines if database initialization is required.
-    // initDB()
-    // defer db.Close()
+	// Uncomment the following lines if database initialization is required.
+	// initDB()
+	// defer db.Close()
 
-    http.HandleFunc("/", formHandler)
-    http.HandleFunc("/submit", submitHandler)
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", formHandler)
+	http.HandleFunc("/submit", submitHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-    fmt.Println("Server started at :8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
